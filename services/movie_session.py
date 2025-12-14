08972143d0568c32b9810cf822e53348a8bf048b
Hanna Svirina <26anna07@ukr.net>
2025-12-14 21:19:17 +0100

    'Solution'

---
+++ services/movie_session.py
@@ -0,0 +1,63 @@
+from db.models import Movie, CinemaHall, MovieSession
+from typing import Optional, List
+from datetime import datetime
+from django.utils import timezone
+
+
+def create_movie_session(
+    movie_show_time: datetime,
+    movie_id: int,
+    cinema_hall_id: int
+) -> MovieSession:
+    if timezone.is_naive(movie_show_time):
+        movie_show_time = timezone.make_aware(movie_show_time)
+
+    movie = Movie.objects.get(id=movie_id)
+    hall = CinemaHall.objects.get(id=cinema_hall_id)
+    session = MovieSession.objects.create(
+        show_time=movie_show_time,
+        movie=movie,
+        cinema_hall=hall
+    )
+    return session
+
+
+def get_movies_sessions(
+    session_date: Optional[str] = None
+) -> List[MovieSession]:
+    if session_date is None:
+        return list(MovieSession.objects.all())
+
+    date_obj = datetime.strptime(session_date, "%Y-%m-%d").date()
+    return list(MovieSession.objects.filter(show_time__date=date_obj))
+
+
+def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+    return MovieSession.objects.get(id=movie_session_id)
+
+
+def update_movie_session(
+    session_id: int,
+    show_time: Optional[datetime] = None,
+    movie_id: Optional[int] = None,
+    cinema_hall_id: Optional[int] = None
+) -> MovieSession:
+    session = MovieSession.objects.get(id=session_id)
+
+    if show_time is not None:
+        session.show_time = show_time
+
+    if movie_id is not None:
+        session.movie = Movie.objects.get(id=movie_id)
+
+    if cinema_hall_id is not None:
+        session.cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+
+    session.save()
+    return session
+
+
+def delete_movie_session_by_id(session_id: int) -> MovieSession:
+    session = MovieSession.objects.get(id=session_id)
+    session.delete()
+    return session
